d6e1643ba0acc4bedf0100a19f18e2e60259b23f
PALIBINA_VALERIA <kler_lynx.ru@mail.ru>
2022-02-13 22:15:34 +0400

    Открывает гугл

---
+++ test2022/src/test/java/tests/TestCase1.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase1 {
+
+    @Test
+    public void test1() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.quit();
+
+    }
+}
